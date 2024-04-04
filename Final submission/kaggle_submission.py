0561017c69151ab33161ada059734f783ca1cf56
MartvStraten <131674849+MartvStraten@users.noreply.github.com>
2024-04-04 11:53:09 +0200

    Final update

--- Final submission/kaggle_submission.py
+++ Final submission/kaggle_submission.py
@@ -12,20 +12,15 @@ import glob
 import pandas as pd
 from matplotlib.pyplot import imread
 from tensorflow.keras import backend
-from main_util import Model_architecture
+from tensorflow.keras.models import load_model
 
 #Change these variables to point at the locations and names of the test dataset and your models.
 model_name = "cnn_augmented_1"
-
 TEST_PATH = 'C:/Users/20212077/OneDrive - TU Eindhoven/Desktop/8P361 - DBL AI for MIA/8p361-project-imaging_gr2/data/test' 
-WEIGHTS_FILEPATH = f"C:/Users/20212077/OneDrive - TU Eindhoven/Desktop/8P361 - DBL AI for MIA/8p361-project-imaging_gr2/Final submission/trained_models/{model_name}_weights.hdf5"
+SAVE_FILEPATH = f"C:/Users/20212077/OneDrive - TU Eindhoven/Desktop/8P361 - DBL AI for MIA/8p361-project-imaging_gr2/Final submission/trained_models/{model_name}.tf"
 
 # load weights into new model
-model = Model_architecture()
-model.create_cnn(kernel_size=(3,3), pool_size=(4,4), first_filters=32, second_filters=64)
-model.compile_cnn(learning_rate=0.001)
-
-model.load_weights(WEIGHTS_FILEPATH)
+model = load_model(SAVE_FILEPATH)
 
 # open the test set in batches (as it is a very big dataset) and make predictions
 test_files = glob.glob(os.path.join(TEST_PATH, '*.tif'))
